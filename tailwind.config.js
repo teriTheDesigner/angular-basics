9d7e0d47f088c31f2873d93a9a4c867dac15f2a0
teriTheDesigner <tere1109@stud.kea.dk>
2024-11-20 14:41:17 +0100

    changing tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
       colors: {
         primary1: "var(--primary1)", // Reference CSS variable
         primary2: "var(--primary2)",
+        green: {
+          50: "#FDF2F8",
+          100: "#FCE7F3",
+          200: "#FBCFE8",
+          300: "#F9A8D4",
+          400: "#F472B6",
+          500: "#EC4899",
+          600: "#DB2777",
+          700: "#BE185D",
+          800: "#9D174D",
+          900: "#831843",
+        },
       },
     },
   },
